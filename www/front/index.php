ff0bf01864ffa2db32271aa0c8e3bc36c69aacff
denan895 <denan895@student.otago.ac.nz>
2024-09-08 17:40:35 +1200

    made the front facing site better looking

--- www/front/index.php
+++ www/front/index.php
@@ -10,45 +10,42 @@
     <h1>My Bookmarks</h1>
 
     <?php
-
-    $serverhost   = '192.168.56.12';
-    $dbname   = 'bookmark_tool';
-    $username   = 'webuser';
+    $serverhost = '192.168.56.12';
+    $dbname = 'bookmark_tool';
+    $username = 'webuser';
     $password = 'lolpassword';
 
-    // create connection
+    // Create connection
     $conn = new mysqli($serverhost, $username, $password, $dbname);
 
-    // check connection
+    // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
     // Query to fetch bookmarks with associated tags
     $sql = "
-    SELECT b.bookmark_id, b.url, b.title, b.description, GROUP_CONCAT(t.tag_name SEPARATOR ', ') AS tags
-    FROM bookmarks b
-    LEFT JOIN bookmark_tags bt ON b.bookmark_id = bt.bookmark_id
-    LEFT JOIN tags t ON bt.tag_id = t.tag_id
-    GROUP BY b.bookmark_id
-";
+        SELECT b.bookmark_id, b.url, b.title, b.description, GROUP_CONCAT(t.tag_name SEPARATOR ', ') AS tags
+        FROM bookmarks b
+        LEFT JOIN bookmark_tags bt ON b.bookmark_id = bt.bookmark_id
+        LEFT JOIN tags t ON bt.tag_id = t.tag_id
+        GROUP BY b.bookmark_id
+    ";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        echo "<table>";
-        echo "<tr><th>Title</th><th>URL</th><th>Description</th><th>Tags</th></tr>";
+        echo "<div class='bookmarks-grid'>";
 
-        // Output data of each row
+        // Output data of each row as a card
         while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>" . htmlspecialchars($row["title"]) . "</td>";
-            echo "<td><a href='" . htmlspecialchars($row["url"]) . "' target='_blank'>" . htmlspecialchars($row["url"]) . "</a></td>";
-            echo "<td>" . htmlspecialchars($row["description"]) . "</td>";
-            echo "<td>" . htmlspecialchars($row["tags"]) . "</td>";
-            echo "</tr>";
+            echo "<div class='bookmark-card'>";
+            echo "<div class='bookmark-title'><a href='" . htmlspecialchars($row["url"]) . "' target='_blank'>" . htmlspecialchars($row["title"]) . "</a></div>";
+            echo "<div class='bookmark-description'>" . htmlspecialchars($row["description"]) . "</div>";
+            echo "<div class='bookmark-tags'>Tags: " . htmlspecialchars($row["tags"]) . "</div>";
+            echo "</div>";
         }
 
-        echo "</table>";
+        echo "</div>";
     } else {
         echo "<p>No bookmarks found.</p>";
     }
@@ -57,5 +54,7 @@
     $conn->close();
     ?>
 
-    <h2><a href="http://127.0.0.1:8081/">Click here to add new information</a></h2>
+    <a class="add-btn" href="http://127.0.0.1:8081/">Click here to add new information</a>
 </body>
+
+</html>
